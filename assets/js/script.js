aa3cbad573b4a3740fa9c64423fdb523c57a1e89
CGoddard1 <goddard.cole@gmail.com>
2023-02-20 11:05:36 -0500

    added question/answer array

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,38 @@ function sendMessage() {
     timeEl.textContent = "GAME OVER ";
 }
 setTime();
+
+var questions = [
+    "one",
+    "two",
+    "three",
+    
+];
+
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    }
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parantheses", "square brackets"],
+        answer: "parantheses"
+    }
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    }
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parantheses"],
+        answer: "quotes"
+    }
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    }
+]
